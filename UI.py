45afd8566a456e52a509a2905872b939e376f1c1
João Guedes <jotagw@gmail.com>
2021-06-29 18:02:59 -0300

    Base da interface, leitura dos dados

--- UI.py
+++ UI.py
@@ -1,40 +1,43 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QComboBox, QHBoxLayout, QGroupBox, QVBoxLayout
+from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import *
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 class App(QMainWindow):
     
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 menu - pythonspot.com'
+        self.title = 'PY'
         self.left = 300
         self.top = 100
         self.width = 640
         self.height = 400
-        self.UiComponents()
+        #self.UiComponents()
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('File')
-        editMenu = mainMenu.addMenu('Edit')
-        viewMenu = mainMenu.addMenu('View')
-        searchMenu = mainMenu.addMenu('Search')
-        toolsMenu = mainMenu.addMenu('Tools')
-        helpMenu = mainMenu.addMenu('Help')
+        #mainMenu = self.menuBar()
+        #fileMenu = mainMenu.addMenu('File')
+        #editMenu = mainMenu.addMenu('Edit')
+        #viewMenu = mainMenu.addMenu('View')
+        #searchMenu = mainMenu.addMenu('Search')
+        #toolsMenu = mainMenu.addMenu('Tools')
+        #helpMenu = mainMenu.addMenu('Help')
 
+        self.table_widget = TabsWidget(self)
+        self.setCentralWidget(self.table_widget)
         
+        #exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
+        #exitButton.setShortcut('Ctrl+Q')
+        #exitButton.setStatusTip('Exit application')
+        #exitButton.triggered.connect(self.close)
+        #fileMenu.addAction(exitButton)
         
-        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitButton.setShortcut('Ctrl+Q')
-        exitButton.setStatusTip('Exit application')
-        exitButton.triggered.connect(self.close)
-        fileMenu.addAction(exitButton)
-    
         self.show()
 
     def UiComponents(self):
@@ -50,7 +53,49 @@ class App(QMainWindow):
         self.combo_box.addItems(options_list)
         # setting minimum content length
         self.combo_box.setMinimumContentsLength(3)
-    
+
+class TabsWidget(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        
+        # Criar abas
+        self.tabs = QTabWidget()
+        self.tab_mapa = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(300,200)
+
+        self.tabs.addTab(self.tab_mapa,"Mapa")
+        self.tabs.addTab(self.tab2,"Tab 2")
+        
+        # Modificar 1a aba
+        self.tab_mapa.horizontalGroupBox = QGroupBox("Propriedades")
+        tab_mapa_bt_layout = QHBoxLayout()
+        
+        # Elementos de Input
+        self.tab_mapa.cmb_variavel = QComboBox(self)
+        cmb_variaveis_list = ["Pluviosidade", "Temperatura", "Nebulosidade", "Vel. Média do Vento"]
+        self.tab_mapa.cmb_variavel.addItems(cmb_variaveis_list)
+        tab_mapa_bt_layout.addWidget(self.tab_mapa.cmb_variavel)
+
+        self.tab_mapa.cmb_periodo = QComboBox()
+        cmb_periodo_list = ["Q1", "Q2", "Q3", "Q4"]
+        self.tab_mapa.cmb_periodo.addItems(cmb_periodo_list)
+        tab_mapa_bt_layout.addWidget(self.tab_mapa.cmb_periodo)
+        
+        self.tab_mapa.horizontalGroupBox.setLayout(tab_mapa_bt_layout)
+
+        # Elemento gráfico
+        self.tab_mapa.plot_mapa = Figure()
+
+        self.tab_mapa.layout = QVBoxLayout()
+        self.tab_mapa.layout.addWidget(self.tab_mapa.horizontalGroupBox)
+        self.tab_mapa.setLayout(self.tab_mapa.layout)
+
+        # Adicionar abas
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
--- plot.py
+++ plot.py
@@ -6,6 +6,11 @@ import seaborn as sns
 
 import glob
 
+periodo_Q1 = pd.to_datetime('2020-03-31')
+periodo_Q2 = pd.to_datetime('2020-06-30')
+periodo_Q3 = pd.to_datetime('2020-09-30')
+periodo_Q4 = pd.to_datetime('2020-12-31')
+
 path = "dados_mensais" # use your path
 all_files = glob.glob(path + "/*.csv")
 
@@ -17,6 +22,9 @@ for filename in all_files:
 
 frame = pd.concat(li, axis=0, ignore_index=True)
 frame.data_medicao = pd.to_datetime(frame.data_medicao)
+frame.precipitacao.notnull()
 
-print(frame.head())
-print(frame.info())
+df_Q1 = frame.loc[frame.precipitacao.notnull()].loc[frame.data_medicao <= periodo_Q1].groupby(['latitude', 'longitude', 'altitude'])
+
+print(df_Q1.head())
+#print(frame.info())

1781b48220b84637010dbd46fa1aca6b54e05a64
João Guedes <jotagw@gmail.com>
2021-06-30 19:35:08 -0300

    Visualização de variáveis em associação

--- UI.py
+++ UI.py
@@ -1,6 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QComboBox, QHBoxLayout, QGroupBox, QVBoxLayout
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget, QWidget,
+                            QComboBox, QHBoxLayout, QGroupBox, QVBoxLayout,
+                            QAction)
 from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import QIcon
 import matplotlib as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -23,19 +26,14 @@ class App(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setFixedSize(self.width, self.height)
         
-        #mainMenu = self.menuBar()
-        #fileMenu = mainMenu.addMenu('File')
-        #editMenu = mainMenu.addMenu('Edit')
-        #viewMenu = mainMenu.addMenu('View')
-        #searchMenu = mainMenu.addMenu('Search')
-        #toolsMenu = mainMenu.addMenu('Tools')
-        #helpMenu = mainMenu.addMenu('Help')
+        mainMenu = self.menuBar()
+        helpMenu = mainMenu.addMenu('Ajuda')
         
-        #exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
-        #exitButton.setShortcut('Ctrl+Q')
-        #exitButton.setStatusTip('Exit application')
-        #exitButton.triggered.connect(self.close)
-        #fileMenu.addAction(exitButton)
+        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        helpMenu.addAction(exitButton)
         
         self.table_widget = TabsWidget(self)
         self.setCentralWidget(self.table_widget)
@@ -48,17 +46,19 @@ class TabsWidget(QWidget):
         
         # Objeto com plot e Dataframe
         self.backend = plot.Visualizer()
-        self.variavel = "precipitacao"
+        self.variavel_mapa = "precipitacao"
 
         # Criar abas
         self.tabs = QTabWidget()
         self.tab_mapa = QWidget()
-        self.tab2 = QWidget()
+        self.tab_associacao = QWidget()
         self.tabs.resize(300,200)
 
         self.tabs.addTab(self.tab_mapa,"Mapa")
-        self.tabs.addTab(self.tab2,"Tab 2")
+        self.tabs.addTab(self.tab_associacao,"Associação")
         
+        ### ABAS
+
         # Modificar 1a aba
         self.tab_mapa.horizontalGroupBox = QGroupBox("Propriedades")
         tab_mapa_bt_layout = QHBoxLayout()
@@ -87,7 +87,7 @@ class TabsWidget(QWidget):
         self.tab_mapa.horizontalGroupBox.setLayout(tab_mapa_bt_layout)
 
         # Elementos gráficos
-        self.backend.definir_periodo(0,1, self.variavel)
+        self.backend.definir_periodo(0,1, self.variavel_mapa)
 
         self.tab_mapa.figure = FigureCanvas()
         self.tab_mapa.figure = self.plot_grafico(self.tab_mapa.figure)
@@ -97,13 +97,46 @@ class TabsWidget(QWidget):
         self.tab_mapa.layout.addWidget(self.tab_mapa.figure)
         self.tab_mapa.setLayout(self.tab_mapa.layout)
 
+
+
+        # Modificar 2a aba
+        self.tab_associacao.horizontalGroupBox = QGroupBox("Propriedades")
+        tab_associacao_bt_layout = QHBoxLayout()
+        
+        # Elementos de Input
+        self.tab_associacao.cmb_variavel = []
+        self.tab_associacao.cmb_variavel.append(QComboBox(self))
+        self.tab_associacao.cmb_variavel.append(QComboBox(self))
+        self.tab_associacao.cmb_variavel.append(QComboBox(self))
+        cmb_variaveis_list = ["Precipitação", "Temperatura", "Nebulosidade", "Vel. Média do Vento"]
+        for cmb in self.tab_associacao.cmb_variavel:
+            cmb.addItems(cmb_variaveis_list)
+            tab_associacao_bt_layout.addWidget(cmb)
+            cmb.activated.connect(self.associacao_onchange)
+        self.tab_associacao.cmb_variavel[-1].addItems(["Nenhuma"])
+        self.tab_associacao.horizontalGroupBox.setLayout(tab_associacao_bt_layout)
+
+        # Elementos gráficos
+        self.tab_associacao.figure = FigureCanvas()
+        #self.tab_associacao.figure = self.plot_grafico(self.tab_associacao.figure, associacao=True,
+        #                                                variaveis=["precipitacao", "precipitacao", "precipitacao"])
+
+        self.tab_associacao.layout = QVBoxLayout()
+        self.tab_associacao.layout.addWidget(self.tab_associacao.horizontalGroupBox)
+        self.tab_associacao.layout.addWidget(self.tab_associacao.figure)
+        self.tab_associacao.setLayout(self.tab_associacao.layout)
+
+
+
         # Adicionar abas
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
 
-    def plot_grafico(self, figure_canvas):
+    def plot_grafico(self, figure_canvas:FigureCanvas, associacao=False, variaveis=[]):
         # Elemento gráfico
-        figure = self.backend.plot_periodo()
+        if(not associacao): figure = self.backend.plot_periodo_mapa()
+        else:
+            figure = self.backend.plot_associacao(variaveis)
         figure_canvas = FigureCanvas(figure)
         return figure_canvas
 
@@ -112,22 +145,35 @@ class TabsWidget(QWidget):
         if(self.tab_mapa.cmb_periodo_inicio.currentIndex() -1 > self.tab_mapa.cmb_periodo_final.currentIndex()):
             self.tab_mapa.cmb_periodo_final.setCurrentIndex(self.tab_mapa.cmb_periodo_inicio.currentIndex() -1)
         if(self.tab_mapa.cmb_periodo_inicio.currentIndex() == 0):
-            self.backend.definir_periodo(0,self.tab_mapa.cmb_periodo_final.currentIndex(), self.variavel)
+            self.backend.definir_periodo(0,self.tab_mapa.cmb_periodo_final.currentIndex(), self.variavel_mapa)
         else:
-            self.backend.definir_periodo(self.tab_mapa.cmb_periodo_inicio.currentIndex() - 1,self.tab_mapa.cmb_periodo_final.currentIndex(), self.variavel)
+            self.backend.definir_periodo(self.tab_mapa.cmb_periodo_inicio.currentIndex() - 1,self.tab_mapa.cmb_periodo_final.currentIndex(), self.variavel_mapa)
         # Limpar o canvas e adicionar novamente
         self.tab_mapa.layout.removeWidget(self.tab_mapa.figure)
         self.tab_mapa.figure = self.plot_grafico(self.tab_mapa.figure)
         self.tab_mapa.figure.draw()
         self.tab_mapa.layout.addWidget(self.tab_mapa.figure)
 
+    def variavel_filter(self,variavel:int):
+        if(variavel == 0): return "precipitacao"
+        if(variavel == 1): return "temperatura_media"
+        if(variavel == 2): return "nebulosidade"
+        if(variavel == 3): return "vento_media"
+        else: return False
+
     def variavel_onchange(self):
         variavel_pre = self.tab_mapa.cmb_variavel.currentIndex()
-        if(variavel_pre == 0): self.variavel = "precipitacao"
-        if(variavel_pre == 1): self.variavel = "temperatura_media"
-        if(variavel_pre == 2): self.variavel = "nebulosidade"
-        if(variavel_pre == 3): self.variavel = "vento_media"
+        self.variavel_mapa = self.variavel_filter(variavel_pre)
         self.periodo_onchange()
+
+    def associacao_onchange(self):
+        variaveis_list = []
+        for cmb in self.tab_associacao.cmb_variavel:
+            variaveis_list.append(self.variavel_filter(cmb.currentIndex()))
+        self.tab_associacao.layout.removeWidget(self.tab_associacao.figure)
+        self.tab_associacao.figure = self.plot_grafico(self.tab_associacao.figure, associacao=True, variaveis=variaveis_list)
+        self.tab_associacao.figure.draw()
+        self.tab_associacao.layout.addWidget(self.tab_associacao.figure)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ from pandas.core.frame import DataFrame
 import seaborn as sns
 
 class Visualizer():
-    variavel = "precipitacao"
+    variavel_mapa = "precipitacao"
     df = pd.DataFrame()
     df_periodo = pd.DataFrame()
 
@@ -15,6 +15,7 @@ class Visualizer():
         self.df = pd.read_csv(path, index_col=None, header=0, sep=';')
         self.df.data_medicao = pd.to_datetime(self.df.data_medicao)
 
+    # Plot de variável única por geolocação
     def definir_periodo(self, inicio:int, fim:int, variavel):
         ano_inicial = '2020-'
         ano_final = '2020-'
@@ -27,29 +28,38 @@ class Visualizer():
             fim = 1
         periodo_inicial = pd.to_datetime(ano_inicial + str(inicio + 2) + '-01')
         periodo_final = pd.to_datetime(ano_final + str(fim + 2) + '-01')
-        self.variavel = variavel
+        self.variavel_mapa = variavel
         if(inicio == 0):
-            self.df_periodo = self.df.loc[self.df[self.variavel].notnull()].loc[self.df.data_medicao <= periodo_final].groupby(["latitude", "longitude"])[self.variavel].mean().reset_index()
+            self.df_periodo = self.df.loc[self.df[self.variavel_mapa].notnull()].loc[self.df.data_medicao <= periodo_final].groupby(["latitude", "longitude"])[self.variavel_mapa].mean().reset_index()
         else:
-            self.df_periodo = self.df.loc[self.df[self.variavel].notnull()].loc[self.df.data_medicao >= periodo_inicial].loc[self.df.data_medicao <= periodo_final].groupby(["latitude", "longitude"])[self.variavel].mean().reset_index()
+            self.df_periodo = self.df.loc[self.df[self.variavel_mapa].notnull()].loc[self.df.data_medicao >= periodo_inicial].loc[self.df.data_medicao <= periodo_final].groupby(["latitude", "longitude"])[self.variavel_mapa].mean().reset_index()
 
-    def plot_periodo(self):
+    def plot_periodo_mapa(self):
         title = ""
         color = ""
-        if(self.variavel == "precipitacao"):
+        if(self.variavel_mapa == "precipitacao"):
             title = "Precipitação (mm)"
             color = "ch:s=.25,rot=-.25"
-        if(self.variavel == "temperatura_media"):
+        if(self.variavel_mapa == "temperatura_media"):
             title = "Temperatura Média (ºC)"
             color = "Reds"
-        if(self.variavel == "nebulosidade"):
+        if(self.variavel_mapa == "nebulosidade"):
             title = "Nebulosidade (décimos)"
             color = "Greens"
-        if(self.variavel == "vento_media"):
+        if(self.variavel_mapa == "vento_media"):
             title = "Velocidade do vento(m/s)"
             color = "Greys"
         plt.clf()
-        fig = sns.scatterplot(data=self.df_periodo, x="longitude", y="latitude", s=11, hue=self.variavel, palette=sns.color_palette(color, as_cmap=True))
+        fig = sns.scatterplot(data=self.df_periodo, x="longitude", y="latitude", s=11, hue=self.variavel_mapa, palette=sns.color_palette(color, as_cmap=True))
         fig.axis("Off")
         fig.legend(title=title, shadow=True)
+        return fig.get_figure()
+
+    # Plot de váriaveis por associação
+    def plot_associacao(self, variaveis:list):
+        plt.clf()
+        if(variaveis[2] == False): fig = sns.scatterplot(data=self.df, x=variaveis[0], y=variaveis[1], s=11)
+        else: fig = sns.scatterplot(x=self.df[variaveis[0]].loc[self.df[variaveis[0]].notnull()], y=self.df[variaveis[1]].loc[self.df[variaveis[1]].notnull()], s=11, hue=self.df[variaveis[2]].loc[self.df[variaveis[2]].notnull()], palette=sns.color_palette("ch:s=.25,rot=-.25", as_cmap=True))
+        #fig.axis("Off")
+        #fig.legend(title=title, shadow=True)
         return fig.get_figure()

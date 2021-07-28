bdda2203a30eb9a639c237d9f8246c19bcebbda9
João Guedes <jotagw@gmail.com>
2021-07-27 21:50:25 -0300

    Mapa interativo em browser, modelos de regressão e ajustes na legibilidade do código

--- UI.py
+++ UI.py
@@ -1,179 +1,211 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget, QWidget,
-                            QComboBox, QHBoxLayout, QGroupBox, QVBoxLayout,
-                            QAction)
-from PyQt5 import QtGui, QtCore
+                             QComboBox, QHBoxLayout, QGroupBox, QVBoxLayout,
+                             QAction, QPushButton, QCheckBox)
 from PyQt5.QtGui import QIcon
-import matplotlib as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
 import plot
+import webbrowser
+
 
 class App(QMainWindow):
-    
+
     def __init__(self):
         super().__init__()
         self.title = 'PY'
-        self.left = 300
-        self.top = 100
+        self.left = 350
+        self.top = 50
         self.width = 640
         self.height = 600
-        #self.UiComponents()
+        # self.UiComponents()
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setFixedSize(self.width, self.height)
-        
-        mainMenu = self.menuBar()
-        helpMenu = mainMenu.addMenu('Ajuda')
-        
-        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitButton.setShortcut('Ctrl+Q')
-        exitButton.setStatusTip('Exit application')
-        exitButton.triggered.connect(self.close)
-        helpMenu.addAction(exitButton)
-        
+
+        main_menu = self.menuBar()
+        help_menu = main_menu.addMenu('Ajuda')
+
+        btn_exit = QAction(QIcon('exit24.png'), 'Exit', self)
+        btn_exit.setShortcut('Ctrl+Q')
+        btn_exit.setStatusTip('Exit application')
+        btn_exit.triggered.connect(self.close)
+        help_menu.addAction(btn_exit)
+
         self.table_widget = TabsWidget(self)
         self.setCentralWidget(self.table_widget)
         self.show()
+
+
+def variable_filter(variable: int):
+    if variable == 0:
+        return "precipitacao"
+    if variable == 1:
+        return "temperatura_media"
+    if variable == 2:
+        return "nebulosidade"
+    if variable == 3:
+        return "vento_media"
+    else:
+        return False
+
 
 class TabsWidget(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
-        
+
         # Objeto com plot e Dataframe
         self.backend = plot.Visualizer()
-        self.variavel_mapa = "precipitacao"
+        self.variable_map = "precipitacao"
 
         # Criar abas
         self.tabs = QTabWidget()
-        self.tab_mapa = QWidget()
-        self.tab_associacao = QWidget()
-        self.tabs.resize(300,200)
+        self.tab_map = QWidget()
+        self.tab_association = QWidget()
+        self.tabs.resize(300, 200)
 
-        self.tabs.addTab(self.tab_mapa,"Mapa")
-        self.tabs.addTab(self.tab_associacao,"Associação")
-        
-        ### ABAS
+        self.tabs.addTab(self.tab_map, "Mapa")
+        self.tabs.addTab(self.tab_association, "Associação")
+
+        # ABAS
 
         # Modificar 1a aba
-        self.tab_mapa.horizontalGroupBox = QGroupBox("Propriedades")
-        tab_mapa_bt_layout = QHBoxLayout()
-        
+        self.tab_map.horizontalGroupBox = QGroupBox("Propriedades")
+        tab_map_bt_layout = QHBoxLayout()
+
         # Elementos de Input
-        self.tab_mapa.cmb_variavel = QComboBox(self)
-        cmb_variaveis_list = ["Precipitação", "Temperatura", "Nebulosidade", "Vel. Média do Vento"]
-        self.tab_mapa.cmb_variavel.addItems(cmb_variaveis_list)
-        tab_mapa_bt_layout.addWidget(self.tab_mapa.cmb_variavel)
-        self.tab_mapa.cmb_variavel.activated.connect(self.variavel_onchange)
+        self.tab_map.cmb_variable = QComboBox(self)
+        cmb_variables_list = ["Precipitação", "Temperatura", "Nebulosidade", "Vel. Média do Vento"]
+        self.tab_map.cmb_variable.addItems(cmb_variables_list)
+        tab_map_bt_layout.addWidget(self.tab_map.cmb_variable)
+        self.tab_map.cmb_variable.activated.connect(self.variable_onchange)
 
-        self.tab_mapa.cmb_periodo_inicio = QComboBox()
-        cmb_periodo_inicio_list = ["Início do ano", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-                                    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-        self.tab_mapa.cmb_periodo_inicio.addItems(cmb_periodo_inicio_list)
-        tab_mapa_bt_layout.addWidget(self.tab_mapa.cmb_periodo_inicio)
-        self.tab_mapa.cmb_periodo_inicio.activated.connect(self.periodo_onchange)
+        self.tab_map.cmb_period_start = QComboBox()
+        cmb_period_start_list = ["Início do ano", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                                 "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        self.tab_map.cmb_period_start.addItems(cmb_period_start_list)
+        tab_map_bt_layout.addWidget(self.tab_map.cmb_period_start)
+        self.tab_map.cmb_period_start.activated.connect(self.period_onchange)
 
-        self.tab_mapa.cmb_periodo_final = QComboBox()
-        cmb_periodo_final_list = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-                                    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-        self.tab_mapa.cmb_periodo_final.addItems(cmb_periodo_final_list)
-        tab_mapa_bt_layout.addWidget(self.tab_mapa.cmb_periodo_final)
-        self.tab_mapa.cmb_periodo_final.activated.connect(self.periodo_onchange)
-        
-        self.tab_mapa.horizontalGroupBox.setLayout(tab_mapa_bt_layout)
+        self.tab_map.cmb_period_end = QComboBox()
+        cmb_period_end_list = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                               "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        self.tab_map.cmb_period_end.addItems(cmb_period_end_list)
+        tab_map_bt_layout.addWidget(self.tab_map.cmb_period_end)
+        self.tab_map.cmb_period_end.activated.connect(self.period_onchange)
+
+        self.tab_map.horizontalGroupBox.setLayout(tab_map_bt_layout)
+
+        self.tab_map.btn_browser = QPushButton()
+        self.tab_map.btn_browser.setText("Abrir no navegador")
+        self.tab_map.btn_browser.clicked.connect(self.browser_onclick)
 
         # Elementos gráficos
-        self.backend.definir_periodo(0,1, self.variavel_mapa)
+        self.backend.set_period(0, 1, self.variable_map)
 
-        self.tab_mapa.figure = FigureCanvas()
-        self.tab_mapa.figure = self.plot_grafico(self.tab_mapa.figure)
+        self.tab_map.figure = FigureCanvas()
+        self.tab_map.figure = self.plot_graph(self.tab_map.figure)
 
-        self.tab_mapa.layout = QVBoxLayout()
-        self.tab_mapa.layout.addWidget(self.tab_mapa.horizontalGroupBox)
-        self.tab_mapa.layout.addWidget(self.tab_mapa.figure)
-        self.tab_mapa.setLayout(self.tab_mapa.layout)
-
-
+        self.tab_map.layout = QVBoxLayout()
+        self.tab_map.layout.addWidget(self.tab_map.horizontalGroupBox)
+        self.tab_map.layout.addWidget(self.tab_map.btn_browser)
+        self.tab_map.layout.addWidget(self.tab_map.figure)
+        self.tab_map.setLayout(self.tab_map.layout)
 
         # Modificar 2a aba
-        self.tab_associacao.horizontalGroupBox = QGroupBox("Propriedades")
-        tab_associacao_bt_layout = QHBoxLayout()
-        
+        self.tab_association.horizontalGroupBox = QGroupBox("Propriedades")
+        tab_association_bt_layout = QHBoxLayout()
+
         # Elementos de Input
-        self.tab_associacao.cmb_variavel = []
-        self.tab_associacao.cmb_variavel.append(QComboBox(self))
-        self.tab_associacao.cmb_variavel.append(QComboBox(self))
-        self.tab_associacao.cmb_variavel.append(QComboBox(self))
-        cmb_variaveis_list = ["Precipitação", "Temperatura", "Nebulosidade", "Vel. Média do Vento"]
-        for cmb in self.tab_associacao.cmb_variavel:
-            cmb.addItems(cmb_variaveis_list)
-            tab_associacao_bt_layout.addWidget(cmb)
-            cmb.activated.connect(self.associacao_onchange)
-        self.tab_associacao.cmb_variavel[-1].addItems(["Nenhuma"])
-        self.tab_associacao.horizontalGroupBox.setLayout(tab_associacao_bt_layout)
+        self.tab_association.cmb_variable = []
+        self.tab_association.cmb_variable.append(QComboBox(self))
+        self.tab_association.cmb_variable.append(QComboBox(self))
+        self.tab_association.cmb_variable.append(QComboBox(self))
+        cmb_variables_list = ["Precipitação", "Temperatura", "Nebulosidade", "Vel. Média do Vento"]
+        for cmb in self.tab_association.cmb_variable:
+            cmb.addItems(cmb_variables_list)
+            tab_association_bt_layout.addWidget(cmb)
+            cmb.activated.connect(self.association_onchange)
+        self.tab_association.cmb_variable[-1].addItems(["Nenhuma"])
+        self.tab_association.horizontalGroupBox.setLayout(tab_association_bt_layout)
+
+        self.tab_association.horizontalGroupBoxOptions = QGroupBox("Opções")
+        tab_association_chb_layout = QHBoxLayout()
+        self.tab_association.chb_regression_line = QCheckBox()
+        self.tab_association.chb_regression_line.setText("Modelo de regressão")
+        self.tab_association.chb_regression_line.stateChanged.connect(self.association_onchange)
+
+        self.tab_association.cmb_regression_model = QComboBox()
+        self.tab_association.cmb_regression_model.addItems(["Regressão linear", "Regressão de Lowess"])
+        self.tab_association.cmb_regression_model.activated.connect(
+            lambda: self.association_onchange() if self.tab_association.chb_regression_line.isChecked() else None)
+        # inline if para economizar um gráfico se a regressão está desativada
+
+        tab_association_chb_layout.addWidget(self.tab_association.chb_regression_line)
+        tab_association_chb_layout.addWidget(self.tab_association.cmb_regression_model)
+        self.tab_association.horizontalGroupBoxOptions.setLayout(tab_association_chb_layout)
 
         # Elementos gráficos
-        self.tab_associacao.figure = FigureCanvas()
-        #self.tab_associacao.figure = self.plot_grafico(self.tab_associacao.figure, associacao=True,
-        #                                                variaveis=["precipitacao", "precipitacao", "precipitacao"])
+        self.tab_association.figure = FigureCanvas()
 
-        self.tab_associacao.layout = QVBoxLayout()
-        self.tab_associacao.layout.addWidget(self.tab_associacao.horizontalGroupBox)
-        self.tab_associacao.layout.addWidget(self.tab_associacao.figure)
-        self.tab_associacao.setLayout(self.tab_associacao.layout)
-
-
+        self.tab_association.layout = QVBoxLayout()
+        self.tab_association.layout.addWidget(self.tab_association.horizontalGroupBox)
+        self.tab_association.layout.addWidget(self.tab_association.horizontalGroupBoxOptions)
+        self.tab_association.layout.addWidget(self.tab_association.figure)
+        self.tab_association.setLayout(self.tab_association.layout)
 
         # Adicionar abas
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
 
-    def plot_grafico(self, figure_canvas:FigureCanvas, associacao=False, variaveis=[]):
+    def plot_graph(self, figure_canvas: FigureCanvas, association=False, variables=[]):
         # Elemento gráfico
-        if(not associacao): figure = self.backend.plot_periodo_mapa()
+        if not association:
+            figure = self.backend.plot_period_map()
         else:
-            figure = self.backend.plot_associacao(variaveis)
+            figure = self.backend.plot_association(variables)
         figure_canvas = FigureCanvas(figure)
         return figure_canvas
 
-    def periodo_onchange(self):
-        # Condição para primeiro periodo ou intervalo
-        if(self.tab_mapa.cmb_periodo_inicio.currentIndex() -1 > self.tab_mapa.cmb_periodo_final.currentIndex()):
-            self.tab_mapa.cmb_periodo_final.setCurrentIndex(self.tab_mapa.cmb_periodo_inicio.currentIndex() -1)
-        if(self.tab_mapa.cmb_periodo_inicio.currentIndex() == 0):
-            self.backend.definir_periodo(0,self.tab_mapa.cmb_periodo_final.currentIndex(), self.variavel_mapa)
+    def period_onchange(self):
+        # Condição para primeiro period ou intervalo
+        if self.tab_map.cmb_period_start.currentIndex() - 1 > self.tab_map.cmb_period_end.currentIndex():
+            self.tab_map.cmb_period_end.setCurrentIndex(self.tab_map.cmb_period_start.currentIndex() - 1)
+        if self.tab_map.cmb_period_start.currentIndex() == 0:
+            self.backend.set_period(0, self.tab_map.cmb_period_end.currentIndex(), self.variable_map)
         else:
-            self.backend.definir_periodo(self.tab_mapa.cmb_periodo_inicio.currentIndex() - 1,self.tab_mapa.cmb_periodo_final.currentIndex(), self.variavel_mapa)
+            self.backend.set_period(self.tab_map.cmb_period_start.currentIndex() - 1,
+                                    self.tab_map.cmb_period_end.currentIndex(), self.variable_map)
         # Limpar o canvas e adicionar novamente
-        self.tab_mapa.layout.removeWidget(self.tab_mapa.figure)
-        self.tab_mapa.figure = self.plot_grafico(self.tab_mapa.figure)
-        self.tab_mapa.figure.draw()
-        self.tab_mapa.layout.addWidget(self.tab_mapa.figure)
+        self.tab_map.layout.removeWidget(self.tab_map.figure)
+        self.tab_map.figure = self.plot_graph(self.tab_map.figure)
+        self.tab_map.figure.draw()
+        self.tab_map.layout.addWidget(self.tab_map.figure)
 
-    def variavel_filter(self,variavel:int):
-        if(variavel == 0): return "precipitacao"
-        if(variavel == 1): return "temperatura_media"
-        if(variavel == 2): return "nebulosidade"
-        if(variavel == 3): return "vento_media"
-        else: return False
+    def variable_onchange(self):
+        variable_pre = self.tab_map.cmb_variable.currentIndex()
+        self.variable_map = variable_filter(variable_pre)
+        self.period_onchange()
 
-    def variavel_onchange(self):
-        variavel_pre = self.tab_mapa.cmb_variavel.currentIndex()
-        self.variavel_mapa = self.variavel_filter(variavel_pre)
-        self.periodo_onchange()
+    def association_onchange(self):
+        variables_list = []
+        for cmb in self.tab_association.cmb_variable:
+            variables_list.append(variable_filter(cmb.currentIndex()))
+        # Últimos índices das variáveis são o modelo de regressão
+        variables_list.append(self.tab_association.chb_regression_line.isChecked())
+        variables_list.append(self.tab_association.cmb_regression_model.currentIndex())
+        self.tab_association.layout.removeWidget(self.tab_association.figure)
+        self.tab_association.figure = self.plot_graph(self.tab_association.figure, association=True,
+                                                      variables=variables_list)
+        self.tab_association.figure.draw()
+        self.tab_association.layout.addWidget(self.tab_association.figure)
 
-    def associacao_onchange(self):
-        variaveis_list = []
-        for cmb in self.tab_associacao.cmb_variavel:
-            variaveis_list.append(self.variavel_filter(cmb.currentIndex()))
-        self.tab_associacao.layout.removeWidget(self.tab_associacao.figure)
-        self.tab_associacao.figure = self.plot_grafico(self.tab_associacao.figure, associacao=True, variaveis=variaveis_list)
-        self.tab_associacao.figure.draw()
-        self.tab_associacao.layout.addWidget(self.tab_associacao.figure)
+    def browser_onclick(self):
+        webbrowser.open(self.backend.plot_map_web(), new=0)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- plot.py
+++ plot.py
@@ -1,14 +1,17 @@
-from os import sep
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from pandas.core.frame import DataFrame
+# import numpy as np
 import seaborn as sns
+import folium
+import io
+import os
+from scipy import stats
 
-class Visualizer():
-    variavel_mapa = "precipitacao"
+
+class Visualizer:
+    variable_map = "precipitacao"
     df = pd.DataFrame()
-    df_periodo = pd.DataFrame()
+    df_period = pd.DataFrame()
 
     def __init__(self):
         path = "dados_mensais/dados_mensais.csv"
@@ -16,50 +19,119 @@ class Visualizer():
         self.df.data_medicao = pd.to_datetime(self.df.data_medicao)
 
     # Plot de variável única por geolocação
-    def definir_periodo(self, inicio:int, fim:int, variavel):
-        ano_inicial = '2020-'
-        ano_final = '2020-'
-        if(inicio != 0 and inicio == fim): inicio -= 1
-        if(inicio > 10):
-            ano_inicial = '2021-'
-            inicio = 1
-        if(fim > 10):
-            ano_final= '2021-'
-            fim = 1
-        periodo_inicial = pd.to_datetime(ano_inicial + str(inicio + 2) + '-01')
-        periodo_final = pd.to_datetime(ano_final + str(fim + 2) + '-01')
-        self.variavel_mapa = variavel
-        if(inicio == 0):
-            self.df_periodo = self.df.loc[self.df[self.variavel_mapa].notnull()].loc[self.df.data_medicao <= periodo_final].groupby(["latitude", "longitude"])[self.variavel_mapa].mean().reset_index()
+    def set_period(self, start: int, end: int, variable):
+        year_start = '2020-'
+        year_end = '2020-'
+        if start != 0 and start == end:
+            start -= 1
+        if start > 10:
+            year_start = '2021-'
+            start = 1
+        if end > 10:
+            year_end = '2021-'
+            end = 1
+        period_start = pd.to_datetime(year_start + str(start + 2) + '-01')
+        period_end = pd.to_datetime(year_end + str(end + 2) + '-01')
+        self.variable_map = variable
+        if start == 0:
+            self.df_period = \
+                self.df.loc[self.df[self.variable_map].notnull()].loc[self.df.data_medicao <= period_end].groupby(
+                    ["latitude", "longitude"])[self.variable_map].mean().reset_index()
         else:
-            self.df_periodo = self.df.loc[self.df[self.variavel_mapa].notnull()].loc[self.df.data_medicao >= periodo_inicial].loc[self.df.data_medicao <= periodo_final].groupby(["latitude", "longitude"])[self.variavel_mapa].mean().reset_index()
+            self.df_period = \
+                self.df.loc[self.df[self.variable_map].notnull()].loc[self.df.data_medicao >= period_start].loc[
+                    self.df.data_medicao <= period_end].groupby(["latitude", "longitude"])[
+                    self.variable_map].mean().reset_index()
 
-    def plot_periodo_mapa(self):
+    def plot_period_map(self):
+        title, color = get_variable_title(self.variable_map)
+        plt.clf()
+        graph = sns.scatterplot(data=self.df_period, x="longitude", y="latitude", s=11, hue=self.variable_map,
+                                palette=sns.color_palette(color, as_cmap=True))
+        graph.set_ylim(bottom=-35, top=5)
+        graph.set_xlim(left=-72, right=-34)
+        graph.axis("Off")
+        graph.legend(title=title, shadow=True)
+        return graph.get_figure()
+
+    # Plot de variáveis por associação
+    def plot_association(self, variables: list):
+        plt.clf()
+        df_association = self.df[self.df[variables[0]] != 0].loc[self.df[variables[1]] != 0].dropna()
+        slope, intercept, r_value, p_value, std_err = stats.linregress(df_association[variables[0]],df_association[variables[1]])
+        if variables[-2]:
+            graph = sns.lmplot(data=df_association,
+                               x=variables[0],
+                               y=variables[1],
+                               line_kws={'color': 'red'},
+                               lowess=variables[-1],
+                               height=4,
+                               legend="coeficiente: {0:.2f}".format(slope))
+            #graph.set_xlabel(get_variable_title(variables[0])[0])
+            #graph.set_ylabel(get_variable_title(variables[1])[0])
+            return graph.fig
+        else:
+            if not variables[2]:
+                graph = sns.scatterplot(data=df_association,
+                                        x=variables[0],
+                                        y=variables[1], s=11)
+            else:
+                graph = sns.scatterplot(data=df_association,
+                                        x=variables[0],
+                                        y=variables[1], s=11,
+                                        hue=variables[2],
+                                        palette=sns.color_palette("ch:s=.25,rot=-.25", as_cmap=True))
+        graph.set_xlabel(get_variable_title(variables[0])[0])
+        graph.set_ylabel(get_variable_title(variables[1])[0])
+        return graph.get_figure()
+
+    def plot_map_web(self):
+        max_value = self.df_period[self.variable_map].max()
+        min_value = self.df_period[self.variable_map].min()
+
+        if self.variable_map == "precipitacao":
+            color_str = "rgba(35,100,200,"
+        elif self.variable_map == "temperatura_media":
+            color_str = "rgba(145,35,10,"
+        elif self.variable_map == "nebulosidade":
+            color_str = "rgba(35,150,35,"
+        elif self.variable_map == "vento_media":
+            color_str = "rgba(50,50,50,"
+
+        m = folium.Map(location=[-15.7648084, -47.8878119],
+                       zoom_start=5)
+        for row in self.df_period.iterrows():
+            popup_text = '{}: {}'
+            popup_text = popup_text.format(self.variable_map,
+                                           row[self.variable_map])
+            folium.CircleMarker([row['latitude'], row['longitude']],
+                                radius=2,
+                                fill=True,
+                                color=color_str +
+                                      str((row[self.variable_map] - min_value) / (max_value - min_value)) +
+                                      ")",
+                                popup=popup_text).add_to(m)
+
+        data = io.BytesIO()
+        m.save(data, close_file=False)
+        f = open("folium_map.html", 'w')
+        f.write(data.getvalue().decode())
+        f.close()
+        return "file:///" + os.getcwd() + '/' + "folium_map.html"
+
+def get_variable_title(variable):
         title = ""
         color = ""
-        if(self.variavel_mapa == "precipitacao"):
+        if variable == "precipitacao":
             title = "Precipitação (mm)"
             color = "ch:s=.25,rot=-.25"
-        if(self.variavel_mapa == "temperatura_media"):
+        if variable == "temperatura_media":
             title = "Temperatura Média (ºC)"
             color = "Reds"
-        if(self.variavel_mapa == "nebulosidade"):
+        if variable == "nebulosidade":
             title = "Nebulosidade (décimos)"
             color = "Greens"
-        if(self.variavel_mapa == "vento_media"):
+        if variable == "vento_media":
             title = "Velocidade do vento(m/s)"
             color = "Greys"
-        plt.clf()
-        fig = sns.scatterplot(data=self.df_periodo, x="longitude", y="latitude", s=11, hue=self.variavel_mapa, palette=sns.color_palette(color, as_cmap=True))
-        fig.axis("Off")
-        fig.legend(title=title, shadow=True)
-        return fig.get_figure()
-
-    # Plot de váriaveis por associação
-    def plot_associacao(self, variaveis:list):
-        plt.clf()
-        if(variaveis[2] == False): fig = sns.scatterplot(data=self.df, x=variaveis[0], y=variaveis[1], s=11)
-        else: fig = sns.scatterplot(x=self.df[variaveis[0]].loc[self.df[variaveis[0]].notnull()], y=self.df[variaveis[1]].loc[self.df[variaveis[1]].notnull()], s=11, hue=self.df[variaveis[2]].loc[self.df[variaveis[2]].notnull()], palette=sns.color_palette("ch:s=.25,rot=-.25", as_cmap=True))
-        #fig.axis("Off")
-        #fig.legend(title=title, shadow=True)
-        return fig.get_figure()
+        return title, color
